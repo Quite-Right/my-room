b326a2badb7548a244d5e435c5d64989935a2d47
Новогрудский Дмитрий Викторович <Novogrudskiy.D.V@sberbank.ru>
2020-10-08 00:05:42 +0300

    added react--typescript template

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const path = require("path")
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
 app.listen(port, () => {
