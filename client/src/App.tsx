d0efdbe4bff41c016d352d8213ed74d44f631084
Новогрудский Дмитрий Викторович <Novogrudskiy.D.V@sberbank.ru>
2020-10-11 20:32:17 +0300

    ädded client folder structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Router */}
+      <Header></Header>
+      {/* Switch */}
+      {/* Route  /v.0.1.0 */}
+      <Main></Main>
+      {/* Route без рута */}
+      <NotFound />
+      {/* Switch end */}
+      <Footer></Footer>
+      {/* Router end */}
     </div>
   );
 }
