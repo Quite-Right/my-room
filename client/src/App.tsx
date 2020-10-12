0fff4a967c1e1ac85ba7e469f94a1ecefe73a5d3
Voevodskiy Egor <voevodskiy.ea@gmail.com>
2020-10-12 17:51:57 +0300

    TS Router created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import './App.css';
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import NotFound from "./components/NotFound";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      {/* Router */}
-      <Header></Header>
-      {/* Switch */}
-      {/* Route  /v.0.1.0 */}
-      <Main></Main>
-      {/* Route без рута */}
-      <NotFound />
-      {/* Switch end */}
-      <Footer></Footer>
-      {/* Router end */}
+
+      <Router>
+        <Header></Header>
+
+        <Switch>
+          <Route path="/v.0.1.0" exact component={Main} />
+          <Route path="" exact component={NotFound} />
+        </Switch>
+
+        <Footer></Footer>
+      </Router>
+
     </div>
   );
 }
