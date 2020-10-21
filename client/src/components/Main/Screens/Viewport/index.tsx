407bc2bb3ffd8d15f86bc891cbc1b9e4c6476fdb
Voevodskiy Egor <voevodskiy.ea@gmail.com>
2020-10-21 17:06:43 +0300

    changed Header Footer, raw version of app

---
+++ client/src/components/Main/Screens/Viewport/index.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { NavLink} from 'react-router-dom';
+
+interface Props {
+
+}
+interface State {
+    toggle?: boolean,
+}
+
+export default class index extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            toggle: true
+        }
+    }
+    render() {
+        const { toggle } = this.state;
+        return (
+            <div onClick={() => { this.setState({ toggle: !toggle }) }}>
+                <div >
+                    123
+                </div>
+                <div className={`${toggle ? "__active" : ""}`}>
+                    <NavLink to="/Web" activeClassName="__current">Web</NavLink>
+                    <NavLink to="/Mobile" activeClassName="__current">Mobile</NavLink>
+                    <NavLink to="/Desktop" activeClassName="__current">Desktop</NavLink>
+                    <NavLink to="/Games" activeClassName="__current">Games</NavLink>
+                </div>
+            </div>
+        )
+    }
+}
